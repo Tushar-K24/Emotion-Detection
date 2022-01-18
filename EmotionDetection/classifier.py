8ec1314c9d8d2f1deb24f0cd4f1952f749ad093e
Tushar Khanduri <tushar.04516403219@ipu.ac.in>
2022-01-19 01:15:34 +0530

    fix: function

--- EmotionDetection/classifier.py
+++ EmotionDetection/classifier.py
@@ -1,64 +1,49 @@
-from keras.layers import Layer, Flatten, Dense, Dropout
+from keras.layers import Flatten, Dense, Dropout
 import roi_pooling
 
-class Classifier(Layer):
+def classifier_layer(fmap, input_rois, num_rois, num_classes=4, pool_size=7):
     '''
-    Classifier()
+    classifier_layer()
+        Returns the output classifier with both classification head and regression head
+
+        Note: If needed, change the architecture of the classifier in classifier.py
 
     Arguments
-        None
-    
-    Methods
-        __call__()
-            Returns the output classifier with both classification head and regression head
+        fmap: 
+            an input tensor to pool the features
 
+        input_rois: 
+            a 3D input tensor of shape (1,num_rois,4) where 4 is for (x,y,w,h)
+
+        num_rois: 
+            number of rois for processing
+
+        num_classes:
+            number of classes to output(including background class)
+            Default: 4
+
+        pool_size:
+            pre-defined pool size for fixed size feature vectors
+            Default: 7
+
+    Output
+        a list of two Layers [out_class, out_reg]
+        
+        out_class: Classification head with num_classes classes as output
+        out_reg: Regression head for region refinement
     '''
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-    
-    def __call__(self, fmap, input_rois, num_rois, num_classes=4, pool_size=7):
-        '''
-        __call__()
-            Returns the output classifier with both classification head and regression head
+    pooled_rois = roi_pooling(pool_size, num_rois)([fmap, input_rois])
 
-            Note: If needed, change the architecture of the classifier in classifier.py
+    out = Flatten(name='Flatten')(pooled_rois)
+    out = Dense(4096, activation='relu', name='fc1')(out)
+    out = Dropout(0.5)(out)
+    out = Dense(4096, activation='relu', name='fc2')(out)
+    out = Dropout(0.5)(out)
 
-        Arguments
-            fmap: 
-                an input tensor to pool the features
+    #Classification Head 
+    out_class = Dense(num_classes, activation='softmax', name='class_head')(out)
 
-            input_rois: 
-                a 3D input tensor of shape (1,num_rois,4) where 4 is for (x,y,w,h)
+    #Regression Head
+    out_reg = Dense(4*(num_classes-1), activation='linear', name='regression_head')(out)
 
-            num_rois: 
-                number of rois for processing
-
-            num_classes:
-                number of classes to output(including background class)
-                Default: 4
-
-            pool_size:
-                pre-defined pool size for fixed size feature vectors
-                Default: 7
-
-        Output
-            a list of two Layers [out_class, out_reg]
-            
-            out_class: Classification head with num_classes classes as output
-            out_reg: Regression head for region refinement
-        '''
-        pooled_rois = roi_pooling(pool_size, num_rois)([fmap, input_rois])
-
-        out = Flatten(name='Flatten')(pooled_rois)
-        out = Dense(4096, activation='relu', name='fc1')(out)
-        out = Dropout(0.5)(out)
-        out = Dense(4096, activation='relu', name='fc2')(out)
-        out = Dropout(0.5)(out)
-
-        #Classification Head 
-        out_class = Dense(num_classes, activation='softmax', name='class_head')(out)
-
-        #Regression Head
-        out_reg = Dense(4*(num_classes-1), activation='linear', name='regression_head')(out)
-
-        return [out_class, out_reg]
+    return [out_class, out_reg]
