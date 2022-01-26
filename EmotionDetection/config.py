27429d608a4c7082da77438520a7e91c1b65514a
Tushar Khanduri <tushar.04516403219@ipu.ac.in>
2022-01-27 01:07:53 +0530

    add: trial configuration

--- EmotionDetection/config.py
+++ EmotionDetection/config.py
@@ -9,7 +9,8 @@ base_model = 'vgg'
 
 #anchor boxes
 anchor_box_scales = [128, 256, 512] #anchor box scales
-anchor_box_ratios = [[1,1], [1,math.sqrt(2)], [math.sqrt(2),1]]
+#anchor_box_ratios = [[1,1], [1,math.sqrt(2)], [math.sqrt(2),1]] #anchor box ratios
+anchor_box_ratios = [[1,1], [1,2], [2,1]] #anchor box ratios
 
 #downscaling
 rpn_stride = 16
@@ -18,5 +19,5 @@ rpn_stride = 16
 rpn_max_overlap = 0.7
 rpn_min_overlap = 0.3
 
-#total no. of anchor boxes to process in the mini batch
+#total no. of anchor boxes to process in the mini batch 
 num_samples = 256
